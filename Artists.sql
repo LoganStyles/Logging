2dcd583debb5d0b7541c0ed8df4204e7935cc422
LoganStyles <emmaist23@gmail.com>
2022-08-31 16:22:53 +0100

    feat(logging) : setup artists.sql & artists.sh to create db structure plus sample data

---
+++ Artists.sql
@@ -0,0 +1,23 @@
+﻿CREATE TABLE IF NOT EXISTS "__EFMigrationsHistory" (
+    "MigrationId" TEXT NOT NULL CONSTRAINT "PK___EFMigrationsHistory" PRIMARY KEY,
+    "ProductVersion" TEXT NOT NULL
+);
+
+BEGIN TRANSACTION;
+
+CREATE TABLE "Employees" (
+    "Id" INTEGER NOT NULL CONSTRAINT "PK_Employees" PRIMARY KEY AUTOINCREMENT,
+    "FirstName" TEXT NULL,
+    "LastName" TEXT NULL,
+    "Age" INTEGER NOT NULL
+);
+
+INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('20220831151446_SetupLoggingTutorial', '6.0.2');
+
+INSERT INTO Employees values(1,'Max','Bello',25);
+INSERT INTO Employees values(2,'Francis','Ojukwu',34);
+INSERT INTO Employees values(3,'Martha','Bertha',19);
+
+COMMIT;
+
