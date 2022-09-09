4ddcd5b9cd6cf9b149729a87a3dd593fe20fea7a
LoganStyles <emmaist23@gmail.com>
2022-09-09 15:58:20 +0100

    feat(logging) : modified program.cs and added required using statements

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ class Program
     public static void Main(string[] args)
     {
 
-        var context = new ArtistsContext();
+        using var context = new ArtistsContext();
 
         var employee = new Employee { FirstName = "Susan", LastName = "Fredricks", Age = 28 };
         context.Employees.Add(employee);
